ef9bda691e0b664acbcf9d27b52cd0d9479aa555
Gamana <gamana.rs@gmail.com>
2025-11-19 16:28:41 +0530

    Practice code

---
+++ App/src/App.jsx
@@ -0,0 +1,16 @@
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+
+function App() {
+  return(
+    <div>
+      <h1>Destiny</h1>
+      <Home />
+      <About />
+      <Contact />
+      <h1>Thank you</h1>
+    </div>
+  )
+}
+export default App;
